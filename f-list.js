49619ac6d4bd9dcbc747e49eb57b3bc068e6d22c
Stevo <40282847+MrStevenNg@users.noreply.github.com>
2018-10-07 21:58:18 -0700

    renamed f-list.md to .js (more familiar for pseudocode)

---
+++ f-list.js
@@ -0,0 +1,26 @@
+Start Screen functions
+
+playerName
+noNameCheck
+goBtn
+
+Main Game Screen functions
+
+-Game Area
+
+readyBtn
+resetBtn
+choiceClicks
+compareChoices
+winLossPanel
+topDisplayPanel
+player1Display
+player2Display
+whatRound
+showPlayerName
+
+-Chat Area
+
+chatBtn
+displayGameInfo
+displayChatInfo
